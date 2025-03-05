955a54fd8d97e3331b723cf71c0b23c147c0e124
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-03-05 19:11:32 +0200

    added seo

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,26 @@ import AboutAuthor from '@/sections/AboutAuthor'
 import Reviews from '@/sections/Reviews'
 import FAQ from '@/sections/FAG'
 import Footer from '@/sections/Footer'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Rock Club',
+	description:
+		'ROCK Vocal Club — це ексклюзивний закритий клуб для вокалістів та викладачів, присвячений розвитку та вдосконаленню сучасного, рок та екстрим-вокалу. Якщо ви хочете отримати доступ до професійного навчання, поглибленого розбору технік, практичних вправ та відповідей на свої питання, цей клуб — це саме те, що вам потрібно!',
+	keywords: ['Rock vocal', 'vocal', 'extreme vocal '],
+	openGraph: {
+		title: 'Rock Club',
+		description:
+			'ROCK Vocal Club — це ексклюзивний закритий клуб для вокалістів та викладачів, присвячений розвитку та вдосконаленню сучасного, рок та екстрим-вокалу. Якщо ви хочете отримати доступ до професійного навчання, поглибленого розбору технік, практичних вправ та відповідей на свої питання, цей клуб — це саме те, що вам потрібно!',
+		url: 'https://example.com',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Rock Club',
+		description:
+			'ROCK Vocal Club — це ексклюзивний закритий клуб для вокалістів та викладачів, присвячений розвитку та вдосконаленню сучасного, рок та екстрим-вокалу. Якщо ви хочете отримати доступ до професійного навчання, поглибленого розбору технік, практичних вправ та відповідей на свої питання, цей клуб — це саме те, що вам потрібно!',
+	},
+}
 
 export default function Home() {
 	return (
